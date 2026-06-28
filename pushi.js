4415c2f1a4da04657771f18b284c8c241eba2dbe
João Magalhães <joamag@gmail.com>
2026-06-28 13:34:01 +0100

    version: 0.6.6

--- pushi.js
+++ pushi.js
@@ -140,7 +140,7 @@ var Pushi = function(appKey, options) {
  * The version of the Pushi library.
  * @type {string}
  */
-Pushi.VERSION = "0.6.5";
+Pushi.VERSION = "0.6.6";
 
 Pushi.prototype.init = function(appKey, options, callback) {
     // tries to retrieve any previously existing instance
